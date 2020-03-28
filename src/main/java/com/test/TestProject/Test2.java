e2a7c4f48cffac1e451884d99b13618bf6fa3263
giri <kemburugiriprasad72@gmail.com>
2020-03-29 00:07:17 +0530

    First time commiting this project

---
+++ src/main/java/com/test/TestProject/Test2.java
@@ -0,0 +1,13 @@
+package com.test.TestProject;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("test method is in Test2 class");
+	}
+
+}
